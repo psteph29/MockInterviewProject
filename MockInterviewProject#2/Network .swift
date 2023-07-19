a707ac5c5771e51e547be7c5f563bda5422435eb
psteph29 <paigestephenson29@gmail.com>
2023-07-19 12:04:11 -0600

    first commit

---
+++ MockInterviewProject#2/Network .swift
@@ -0,0 +1,8 @@
+//
+//  Network .swift
+//  MockInterviewProject#2
+//
+//  Created by Paige Stephenson on 7/17/23.
+//
+
+import Foundation
---
+++ MockInterviewProject#2/PersonDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PersonDetailTableViewCell.swift
+//  MockInterviewProject#2
+//
+//  Created by Paige Stephenson on 7/17/23.
+//
+
+import UIKit
+
+class PersonDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
